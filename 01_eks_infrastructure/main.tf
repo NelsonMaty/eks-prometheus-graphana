ab9f91bfe7409e3fdaa554dd0a4a4efe2b46d039
Nelson M. Rios <nelsonmatias9@hotmail.com>
2025-02-25 16:17:26 -0300

    refactor: update EKS infrastructure configuration and remove unused resources

--- 01_eks_infrastructure/main.tf
+++ 01_eks_infrastructure/main.tf
@@ -1,63 +1,91 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "nelson-cluster"
+  region = "us-east-1"
+
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+
+  tags = {
+    Example = local.name
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "19.15.1"
 
-  cluster_name    = "${var.cluster_name}-${var.environment}"
-  cluster_version = "1.28"
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
-
-  cluster_endpoint_public_access  = false
-  cluster_endpoint_private_access = true
-
-  enable_irsa = true
-  enable_cluster_creator_admin_permissions = true
-
-  cluster_security_group_additional_rules = {
-    ingress_from_bastion = {
-      description              = "Allow bastion to API server"
-      protocol                 = "tcp"
-      from_port                = 443
-      to_port                  = 443
-      source_security_group_id = aws_security_group.bastion_sg.id
-      type                     = "ingress"
+  cluster_addons = {
+    coredns = {
+      most_recent = true
     }
-    ingress_http = {
-      description = "Cluster ingress from ALB"
-      protocol    = "tcp"
-      from_port   = 80
-      to_port     = 80
-      type        = "ingress"
-      cidr_blocks = ["0.0.0.0/0"]
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
 
-  cluster_addons = {
-    coredns    = {}
-    kube-proxy = {}
-    vpc-cni    = {}
-    eks-pod-identity-agent = {}
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t2.small"]
+
+    attach_cluster_primary_security_group = true
   }
 
   eks_managed_node_groups = {
-    main = {
-      instance_types = ["t3.small"]
-      capacity_type  = "SPOT"
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
+    nelson-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
 
-      update_config = {
-        max_unavailable = 1
+      instance_types = ["t2.small"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "mundose"
       }
     }
   }
 
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "terraform"
-  }
-
-  authentication_mode = "API"
+  tags = local.tags
 }
