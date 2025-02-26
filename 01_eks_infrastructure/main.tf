9f1530c444d6ea06ec50e2f356e7dab63aefc4c6
Nelson M. Rios <nelsonmatias9@hotmail.com>
2025-02-26 08:09:58 -0300

    docs: Add inline comments to EKS infrastructure Terraform configuration

--- 01_eks_infrastructure/main.tf
+++ 01_eks_infrastructure/main.tf
@@ -1,10 +1,14 @@
+# Deploy an Amazon EKS cluster using the Terraform AWS EKS module
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
+  # Name of the EKS cluster
   cluster_name                   = var.name
+  # Allow public access to the cluster API endpoint
   cluster_endpoint_public_access = true
 
+  # Enable essential Kubernetes addons
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -17,25 +21,28 @@ module "eks" {
     }
   }
 
+  # Associate the cluster with the VPC and subnets
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
+  # Default settings for managed node groups
   eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t2.small"]
+    ami_type       = "AL2_x86_64"  # Amazon Linux 2 AMI
+    instance_types = ["t2.small"]  # Instance type for worker nodes
 
     attach_cluster_primary_security_group = true
   }
 
+  # Define a managed node group for the cluster
   eks_managed_node_groups = {
     nelson-cluster-wg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size     = 1  # Minimum number of worker nodes
+      max_size     = 2  # Maximum number of worker nodes
+      desired_size = 1  # Desired number of worker nodes
 
-      instance_types = ["t2.small"]
-      capacity_type  = "SPOT"
+      instance_types = ["t2.small"]  # Instance type for worker nodes
+      capacity_type  = "SPOT"  # Use spot instances to reduce costs
     }
   }
 }
